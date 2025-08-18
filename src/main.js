4e19446e5d160ad4f789f3264b836928b44feb32
JOAO VICTOR DE SOUZA <joao.vsouza30@senacsp.edu.br>
2025-08-18 17:24:08 -0300

    criação da register

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import renderLoginPage from "./pages/Login.js";
- 
+import renderRegisterPage from "./pages/Register.js"; 
+
 document.addEventListener('DOMContentLoaded', () =>{
-    renderLoginPage();
+    renderRegisterPage();
 });
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import LoginForm from "../components/LoginForm.js";
+import Form from "../components/Form.js";
 import Navbar from "../components/Navbar.js";
  
 export default function renderLoginPage() {
@@ -12,7 +12,7 @@ export default function renderLoginPage() {
     divRoot.innerHTML = '';
 
     const titulo = document.createElement('h1');
-    titulo.textContent = 'Faça o login ou crie uma conta';
+    titulo.textContent = 'Faça o login';
     titulo.className = 'titulo';
     titulo.style.textAlign = 'center';
     titulo.style.marginBottom = '25px'; 
@@ -23,8 +23,16 @@ export default function renderLoginPage() {
     container.style.maxWidth = '400px';
 
     divRoot.appendChild(container);
-    const formulario = LoginForm();
+    const formulario = Form();
+
+    const linkVoltar = document.createElement('a');
+    linkVoltar.textContent = "Não tem uma conta? Cadastra-se";
+    linkVoltar.className = '';
+    linkVoltar.href = 'login.html';
+    linkVoltar.style.textAlign = 'center';
+    linkVoltar.style.margin = '10px';
 
     container.appendChild(titulo); 
     container.appendChild(formulario);
+    container.appendChild(linkVoltar);
 }
